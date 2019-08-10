5f26bff4de30cbf3e2f8336d1e3d09d5833f538e
laridev@gmail.com <laridev@gmail.com>
2019-08-10 01:21:26 +0100

    addinf docker-compose :whale: :muscul:

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const redis   = require('redis');
 
 
 const app = new express();
-const client = redis.createClient();
+const host =  {
+    host : 'redis-server',
+    port : 6379
+}
+const client = redis.createClient(host);
 client.set('visits', 0);
 app.get('/' , (req, res)=>{
     client.get('visits', (err, visits) =>{
-        res.send('Number of visits is ', + visits);
+        res.send('Number of visits is : '+visits);
         client.set('visits', parseInt(visits) + 1);
     })
 })
 
-app.listen(4000 , ()=>{
-    console.log('server is working on port 4000')
+app.listen(4001 , ()=>{
+    console.log('server is working on port 4001')
 })
